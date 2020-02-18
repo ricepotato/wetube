60d9e695938781e5b5e708404f6e9e3176344541
Sukjun Sagong <ricepotato40@gmail.com>
2020-02-18 19:49:32 +0900

    routers

--- app.js
+++ app.js
@@ -2,35 +2,18 @@ import express from "express";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
-import { userRouter } from "./router";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
 
 const app = express();
-
-const handleHome = (req, res) => {
-  //console.log(req);
-  res.send("hello from home.");
-};
-
-const handleProfile = (req, res) => {
-  res.send("you are in profile");
-};
-
-const betweenHome = (req, res, next) => {
-  console.log("I'm between");
-  next();
-};
-
-// add midware globally
-app.get("/", handleHome);
 
 app.use(cookieParser());
 app.use(bodyParser());
 app.use(helmet());
 
-// add midware
-// app.get("/", betweenHome, handleHome);
-
-app.get("/profile", handleProfile);
+app.use("/", globalRouter);
 app.use("/user", userRouter);
+app.use("/video", videoRouter);
 
 export default app;
