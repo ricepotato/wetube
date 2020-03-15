b182427283f65f97a762f92a19004336da6a8480
ricepotato <sukjun40@naver.com>
2020-03-16 00:28:03 +0900

    user detail routes me

--- controllers/userController.js
+++ controllers/userController.js
@@ -79,5 +79,15 @@ export const editProfile = (req, res) =>
   res.render("editProfile", { pageTitle: "Edit Profile" });
 export const changePassword = (req, res) =>
   res.render("changePassword", { pageTitle: "Change Password" });
-export const userDetail = (req, res) =>
-  res.render("userDetail", { pageTitle: "User Detail" });
+
+export const userDetail = async (req, res) => {
+  const {
+    params: { id }
+  } = req;
+  try {
+    const user = await User.findById(id);
+    res.render("userDetail", { pageTitle: "User Detail", user });
+  } catch (error) {
+    res.redirect(routes.home);
+  }
+};
