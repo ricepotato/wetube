1c7469d1dd11c192b2cf542e8f4d5f8f2313543a
ricepotato <sukjun40@naver.com>
2020-03-17 00:45:10 +0900

    change password

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -3,8 +3,9 @@ import routes from "../routes";
 import {
   getEditProfile,
   postEditProfile,
-  changePassword,
-  userDetail
+  getChangePassword,
+  userDetail,
+  postChangePassword
 } from "../controllers/userController";
 import { onlyPrivate, uploadAvatar } from "../middlewares";
 
@@ -13,7 +14,8 @@ const userRouter = express.Router();
 userRouter.get(routes.users, (req, res) => res.send("Users"));
 userRouter.get(routes.editProfile, onlyPrivate, getEditProfile);
 userRouter.post(routes.editProfile, onlyPrivate, uploadAvatar, postEditProfile);
-userRouter.get(routes.changePassword, onlyPrivate, changePassword);
+userRouter.get(routes.changePassword, onlyPrivate, getChangePassword);
+userRouter.post(routes.changePassword, onlyPrivate, postChangePassword);
 userRouter.get(routes.userDetail(), userDetail);
 
 export default userRouter;
