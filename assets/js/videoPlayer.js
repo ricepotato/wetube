7cc411e9de2e80e41d1a9a7cbfa41881b875a861
ricepotato <sukjun40@naver.com>
2020-03-17 23:22:45 +0900

    full screen

--- assets/js/videoPlayer.js
+++ assets/js/videoPlayer.js
@@ -2,6 +2,7 @@ const videoContainer = document.getElementById("jsVideoPlayer");
 const videoPlayer = videoContainer.querySelector("#jsVideoPlayer video");
 const playButton = document.getElementById("jsPlayButton");
 const volumeButton = document.getElementById("jsVolumeButton");
+const fullScreenButton = document.getElementById("jsFullScreen");
 
 function handlerPlayClick(e) {
   if (videoPlayer.paused) {
@@ -23,9 +24,23 @@ function handleVolumeClick(e) {
   }
 }
 
+function exitFullScreen(e) {
+  fullScreenButton.innerHTML = '<i class="fas fa-expand"></i>';
+  fullScreenButton.addEventListener("click", goFullScreen);
+  document.webkitExitFullscreen();
+}
+
+function goFullScreen(e) {
+  videoContainer.webkitRequestFullscreen();
+  fullScreenButton.innerHTML = '<i class="fas fa-compress"></i>';
+  fullScreenButton.removeEventListener("click", goFullScreen);
+  fullScreenButton.addEventListener("click", exitFullScreen);
+}
+
 function init() {
   playButton.addEventListener("click", handlerPlayClick);
   volumeButton.addEventListener("click", handleVolumeClick);
+  fullScreenButton.addEventListener("click", goFullScreen);
 }
 
 if (videoContainer) {
