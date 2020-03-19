28224606cc4dc416a568f91a3573d53e5d0fbcd4
ricepotato <sukjun40@naver.com>
2020-03-19 23:42:45 +0900

    record

--- assets/js/videoRecorder.js
+++ assets/js/videoRecorder.js
@@ -1,24 +1,57 @@
 const recorderContainer = document.getElementById("jsRecordContainer");
 const recordBtn = document.getElementById("jsRecordBtn");
 const videoPreview = document.getElementById("jsVideoPreview");
-const startRecording = async () => {
+
+let streamObject = null;
+let videoRecorder = null;
+
+const handleVideoData = event => {
+  console.log(event);
+  const { data: videoFile } = event;
+  const link = document.createElement("a");
+  link.href = URL.createObjectURL(videoFile);
+  link.download = "recorded.webm";
+  document.body.appendChild(link);
+  link.click();
+};
+
+const stopRecording = () => {
+  videoRecorder.stop();
+  recordBtn.addEventListener("click", getVideo);
+  recordBtn.innerHTML = "Start recording";
+};
+
+const startRecording = () => {
+  videoRecorder = new MediaRecorder(streamObject);
+  videoRecorder.addEventListener("dataavailable", handleVideoData);
+  videoRecorder.start();
+  recordBtn.addEventListener("click", stopRecording);
+};
+
+const getVideo = async () => {
   try {
     const stream = await navigator.mediaDevices.getUserMedia({
-      audio: true,
-      video: { width: 1200, height: 720 }
+      audio: true
+      //video: { width: 1200, height: 720 }
     });
     videoPreview.srcObject = stream;
     videoPreview.play();
     videoPreview.muted = true;
+    recordBtn.innerHTML = "Stop recoding";
+    streamObject = stream;
+    startRecording();
   } catch (error) {
     console.log(error);
     recordBtn.innerHTML = "Can't Recored";
-    recordBtn.removeEventListener("click", startRecording);
+    alert(error);
+    recordBtn.removeEventListener("click", getVideo);
+  } finally {
+    recordBtn.removeEventListener("click", getVideo);
   }
 };
 
 function init() {
-  recordBtn.addEventListener("click", startRecording);
+  recordBtn.addEventListener("click", getVideo);
 }
 
 if (recorderContainer) {
