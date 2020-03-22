250d7f8a11307f7c178cdb8d749cf18aec58a151
ricepotato <sukjun40@naver.com>
2020-03-22 23:53:33 +0900

    add comments

--- assets/js/addComment.js
+++ assets/js/addComment.js
@@ -1,5 +1,20 @@
 import axios from "axios";
 const addCommentForm = document.getElementById("jsAddComment");
+const commentList = document.getElementById("jsCommentList");
+const commentNumber = document.getElementById("jsCommentNumber");
+
+const increaseNumber = () => {
+  commentNumber.innerHTML = parseInt(commentNumber.innerHTML, 10) + 1;
+};
+
+const addComment = comment => {
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  span.innerHTML = comment;
+  li.appendChild(span);
+  commentList.prepend(li);
+  increaseNumber();
+};
 
 const sendCommnet = async comment => {
   const videoId = window.location.href.split("/videos/")[1];
@@ -8,6 +23,9 @@ const sendCommnet = async comment => {
     method: "POST",
     data: { comment }
   });
+  if (response.status == 200) {
+    addComment(comment);
+  }
   console.log(response);
 };
 
