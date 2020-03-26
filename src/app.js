e790cda44804428e5c1cedecbb339eeb84b7281e
ricepotato <sukjun40@naver.com>
2020-03-27 01:00:40 +0900

    production build

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+import "@babel/polyfill";
 import express from "express";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
@@ -5,6 +6,7 @@ import bodyParser from "body-parser";
 import passport from "passport";
 import mongoose from "mongoose";
 import session from "express-session";
+import path from "path";
 import MongoStore from "connect-mongo";
 import { localsMiddleware } from "./middlewares";
 import userRouter from "./routers/userRouter";
@@ -21,8 +23,9 @@ const cookieStore = MongoStore(session);
 
 app.use(helmet());
 app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
 app.use("/uploads", express.static("uploads"));
-app.use("/static", express.static("static"));
+app.use("/static", express.static(path.join(__dirname, "static")));
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
